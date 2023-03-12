0a58f81d3b90c99b83a04113a0037fefe6ffe088
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-03-12 19:41:41 +0100

    added keys

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from youtube_stat import YoutubeStats
-API_KEY = "AIzaSyA3ZEknnxdu6G-9aNjsIp_PE-sCYh7B72I"
-channel_id = "UCbXgNpp0jedKWcQiULLbDTA"
+import keys
 
-yt = YoutubeStats(API_KEY, channel_id)
+
+yt = YoutubeStats(keys.API_KEY, keys.channel_id)
 # print(yt.get_channel_statistics())
 # yt.dump()
 yt.get_channel_video_data()
